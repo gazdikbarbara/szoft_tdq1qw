fbe7aabbe2298b41b5419083dfd0d066314a2339
gazdikbarbara <gazdikbarbara97@gmail.com>
2024-02-29 13:04:12 +0100

    Harmadik gyakorlat

---
+++ Hatvany/Form1.cs
@@ -0,0 +1,26 @@
+namespace Hatvany
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Szamok sz = new Szamok();
+            sz.szám = 1;
+            sz.hatvány = 
+            List<Szamok> listám = new List<Szamok>();
+
+
+
+
+
+
+
+            dataGridView1.DataSource = listám;
+        }
+    }
+}
---
+++ VillogoGomb/SzíneződőGomb.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VillogoGomb
+{
+    internal class SzíneződőGomb : Button
+    {
+        public SzíneződőGomb()
+        {
+            Height = 20;
+            Width = 20;
+            Click += SzíneződőGomb_Click;
+           
+        }
+
+        
+        private void SzíneződőGomb_Click(object? sender, EventArgs e)
+        {
+           BackColor = Color.Fuchsia;
+        }
+
+    }
+}
