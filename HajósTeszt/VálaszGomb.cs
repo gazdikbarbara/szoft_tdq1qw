3301b7f709643e55e210e1c5d834b89695327d53
gazdikbarbara <gazdikbarbara97@gmail.com>
2024-03-14 13:07:21 +0100

    Ötödik gyakorlat

---
+++ HajósTeszt/VálaszGomb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HajósTeszt
+{
+    internal class VálaszGomb: TextBox
+    {
+        public VálaszGomb()
+        {
+            ReadOnly = true;
+            BackColor = Color.Gray;
+            BorderStyle = BorderStyle.None;
+            Multiline = true;
+            Height = 70;
+            Width = 300;
+        }
+    }
+}
